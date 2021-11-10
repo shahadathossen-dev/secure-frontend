70466e895f1790803edc715fcfc9ad0b4f477c6d
Shahadat Hossen <shobujlingdu@gmail.com>
2021-11-10 19:23:00 +0600

    Error pages added

--- layouts/error.vue
+++ layouts/error.vue
@@ -18,8 +18,8 @@
         <div class="row">
           <div class="col-12">
             <!-- 404 Content -->
-            <div class="content-404">
-              <img src="/assets/img/media/404-img.png" data-rjs="2" alt="" />
+            <div v-if="error.statusCode === 404" class="content-404">
+              <img src="/assets/img/media/404.png" data-rjs="2" alt="" />
               <h1>uh-oh page not found...</h1>
               <p>
                 Oops! The page you are looking for does not exist. Please return
@@ -37,6 +37,66 @@
               </nuxt-link>
             </div>
             <!-- End 404 Content -->
+            <!-- 403 Content -->
+            <div v-else-if="error.statusCode === 403" class="content-403">
+              <img src="/assets/img/media/403.png" data-rjs="2" alt="" />
+              <h1>uh-oh page not found...</h1>
+              <p>
+                Oops! The page you are looking for does not exist. Please return
+                <br />
+                to the site’s homepage.
+              </p>
+
+              <nuxt-link :to="localePath('/')" class="btn-link">
+                <img
+                  src="/assets/img/icons/left-arrow.svg"
+                  alt=""
+                  class="svg mr-2"
+                />
+                Go Back Home
+              </nuxt-link>
+            </div>
+            <!-- End 403 Content -->
+            <!-- 401 Content -->
+            <div v-else-if="error.statusCode === 401" class="content-401">
+              <img src="/assets/img/media/401.png" data-rjs="2" alt="" />
+              <h1>uh-oh page not found...</h1>
+              <p>
+                Oops! The page you are looking for does not exist. Please return
+                <br />
+                to the site’s homepage.
+              </p>
+
+              <nuxt-link :to="localePath('/')" class="btn-link">
+                <img
+                  src="/assets/img/icons/left-arrow.svg"
+                  alt=""
+                  class="svg mr-2"
+                />
+                Go Back Home
+              </nuxt-link>
+            </div>
+            <!-- End 401 Content -->
+             <!-- 500 Content -->
+            <div v-else class="content-500">
+              <img src="/assets/img/media/500.png" data-rjs="2" alt="" />
+              <h1>uh-oh page not found...</h1>
+              <p>
+                Oops! The page you are looking for does not exist. Please return
+                <br />
+                to the site’s homepage.
+              </p>
+
+              <nuxt-link :to="localePath('/')" class="btn-link">
+                <img
+                  src="/assets/img/icons/left-arrow.svg"
+                  alt=""
+                  class="svg mr-2"
+                />
+                Go Back Home
+              </nuxt-link>
+            </div>
+            <!-- End 500 Content -->
           </div>
         </div>
       </div>
